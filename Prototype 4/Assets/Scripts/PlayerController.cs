6ecd7f7dd24856377720ead0d0b309f222e7a1a5
brocEdson <bedson@mail.bradley.edu>
2020-11-04 21:24:03 -0600

    Did starting work on prototype

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿/* Broc Edson
+ * Prototype 4
+ * Moves the player
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    public float speed = 5.0f;
+
+    private float forwardInput;
+    private GameObject focalPoint;
+
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        focalPoint = GameObject.FindGameObjectWithTag("FocalPoint");
+    }
+
+
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        playerRB.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+}
---
+++ Prototype 4/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,23 @@
+﻿/* Broc Edson
+ * Prototype 4
+ * Rotates the camera around a fixed point
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float rotationSpeed; 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+}
